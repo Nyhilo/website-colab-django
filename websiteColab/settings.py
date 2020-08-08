23f9a3e6dc4d7370f725942b5b23d6c03b668508
Nyhilo <jacclowa@gmail.com>
2020-08-08 00:43:19 -0700

    Add localhost to list of allowed hosts.

--- websiteColab/settings.py
+++ websiteColab/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'DJANGO_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.167.217.83']
+ALLOWED_HOSTS = ['35.167.217.83', 'localhost']
 
 
 # Application definition

fab712f9b65df9d37ade8b7f245c830cb9b1364d
Nyhilo <jacclowa@gmail.com>
2020-08-08 18:11:54 -0700

    Change test route to demo

--- website/views.py
+++ website/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 def index(request):
      return HttpResponse("Hello, world")
 
-def test(request):
+def demo(request):
      return HttpResponse("This is a test")
